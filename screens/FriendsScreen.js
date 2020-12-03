30dc6097c2817da3afcf9ab0f814d0c1d983818e
Lawrence Macharia <macharialawrence2011@gmail.com>
2020-12-03 11:22:07 +0300

    fixed a bug; I moved call to both possibleFriends and currentFriends closer

--- screens/FriendsScreen.js
+++ screens/FriendsScreen.js
@@ -1,12 +1,59 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useCallback } from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native'
+import { useSelector, useDispatch } from 'react-redux'
 
-const HomeScreen = props => {
+import { friendAdd } from '../store/actions/friends'
+
+const HomeScreen = (props) => {
+
+  const friends = useSelector(state => state.friends)
+  const dispatch = useDispatch()
+
+  const addFriend = (friendIndex) => {
+    dispatch(friendAdd(friendIndex))
+  }
+
   return (
-    <View>
-      <Text>Friends</Text>
+    <View style={styles.constainer}>
+      {
+        friends.possibleFriends.length > 0 ? 
+        (
+          friends.possibleFriends.map((friend, index) => (
+            <View key={friend} style={styles.userContainer}>
+              <Text style={styles.text}>{`${index + 1}. ${friend}`}</Text>
+              <Button
+                title="Add"
+                onPress={() => addFriend(index)}
+              />
+            </View>
+          ))
+        ) :
+        (
+          <Text>You have exhausted the list</Text>
+        )
+
+      }
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  constainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  userContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 5
+  },
+  text: {
+    fontSize: 22,
+    marginEnd: 10
+  }
+})
 
 export default HomeScreen
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,28 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native'
+import { useSelector } from 'react-redux'
 
 const HomeScreen = props => {
+
+  const friends = useSelector(state => state.friends)
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.constainer}>
+      <Text>You have {friends.currentFriends.length} friends.</Text>
+      <Button
+        title="Add some friends"
+        onPress={() => { }}
+      />
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  constainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
 export default HomeScreen
